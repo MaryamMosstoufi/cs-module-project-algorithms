124c95aab50b51e2bd2c06e8663adb29a9757c03
MaryamMosstoufi <maryam@zooxoos.com>
2020-09-17 17:34:44 -0400

    Update eating_cookies.py

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,15 +4,38 @@ Returns: an integer
 '''
 
 
-def eating_cookies(n, ways={0: 1, 1: 1, 2: 2}):
+# def eating_cookies(n, ways={0: 1, 1: 1, 2: 2}):
 
-    if n in ways:
-        return ways[n]
+#     if n in ways:
+#         return ways[n]
 
-    ways[n] = eating_cookies(
-        n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+#     ways[n] = eating_cookies(
+#         n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
 
-    return ways[n]
+#     return ways[n]
+
+
+def eating_cookies(n, a={}):
+    if n == 0 or n == 1:
+        return 1
+    if n == 2:
+        return 2
+    # Our base cases are 0:1, 1:1, 2:2
+    base = [1, 1, 2]
+    # Notice that adding these gives 4, i.e. eating_cookies(3)
+    # Notice sum(1, 2, 4) = 7, i.e. eating_cookies(4)
+    # Notice sum(2, 4, 7) = 13, i.e. eating_cookies(5)
+    # Instead of recursively calling the funciton, let's just store the
+    # values in a list, base, and sum the last three elements
+    # To get to this point, n > 2, so we iterate n-2 times
+    for _ in range(2, n):
+        cookies = base[-3] + base[-2] + base[-1]
+        # Then append the new value to the end of the list to continue iteration
+        base.append(cookies)
+    # Return the last element of the list
+    # return base[-1]
+    # Easier to just return the variable cookies!
+    return cookies
 
 
 if __name__ == "__main__":
